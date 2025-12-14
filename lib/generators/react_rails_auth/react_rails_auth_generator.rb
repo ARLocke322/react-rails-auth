cda6475df20f622a10acf15e8fb7372557d73830
Alex Locke <alocke322@gmail.com>
2025-12-14 08:47:17 +0000

    fix/rubocop-errors

--- lib/generators/react_rails_auth/react_rails_auth_generator.rb
+++ lib/generators/react_rails_auth/react_rails_auth_generator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Generator for React Rails authentication setup
 class ReactRailsAuthGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
   def create_controller_files
