4151a9a53a9388f2701eb9ba570ceddf94ba3fdc
Alex Locke <alocke322@gmail.com>
2025-12-14 07:29:07 +0000

    Initial commit

---
+++ lib/generators/react_rails_auth/react_rails_auth_generator.rb
@@ -0,0 +1,42 @@
+class ReactRailsAuthGenerator < Rails::Generators::NamedBase
+  def create_controller_files
+    template "authentication_concern.rb.tt", "app/controllers/concerns/authentication.rb"
+    template "application_controller.rb.tt", "app/controllers/application_controller.rb"
+    template "passwords_controller.rb.tt", "app/controllers/v1/passwords_controller.rb"
+    template "registrations_controller.rb.tt", "app/controllers/v1/registrations_controller.rb"
+    template "sessions_controller.rb.tt", "app/controllers/v1/sessions_controller.rb"
+  end
+
+  def create_mailer_files
+    template "passwords_mailer.rb.tt", "app/mailers/passwords_mailer.rb"
+  end
+
+  def create_model_files
+    template "current.rb.tt", "app/models/current.rb"
+    template "session.rb.tt", "app/models/session.rb"
+    template "user.rb.tt", "app/models/user.rb"
+  end
+
+  def create_config_files
+    template "routes.rb.tt", "config/routes.rb"
+  end
+
+  def create_migration_files
+    template "create_users.rb.tt", "db/migrate/#{Time.now.utc.strftime('%Y%m%d%H%M%S')}_create_users.rb"
+    sleep 1
+    template "create_sessions.rb.tt", "db/migrate/#{Time.now.utc.strftime('%Y%m%d%H%M%S')}_create_sessions.rb"
+  end
+
+  def create_frontend_files
+    template "LoginForm.tsx.tt", "frontend/src/components/LoginForm.tsx"
+    template "SignupForm.tsx.tt", "frontend/src/components/SignupForm.tsx"
+    template "useAuthStore.ts.tt", "frontend/src/hooks/useAuthStore.ts"
+    template "auth_schema.ts.tt", "frontend/src/schemas/auth.ts"
+    template "auth_service.ts.tt", "frontend/src/services/auth.ts"
+    template "auth_types_index.ts.tt", "frontend/src/types/api/auth/index.ts"
+    template "assertions.ts.tt", "frontend/src/utils/assertions.ts"
+    template "App.tsx.tt", "frontend/src/App.tsx"
+    template "index.ts.tt", "frontend/src/index.ts"
+    template "package.json.tt", "frontend/src/package.json"
+  end
+end
---
+++ lib/react_rails_auth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "react_rails_auth/version"
+
+module ReactRailsAuth
+  class Error < StandardError; end
+  # Your code goes here...
+end
